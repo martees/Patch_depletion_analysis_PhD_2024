f83df19c9e17f82feac745a5625b3025f78ddcfa
martees <alicealasmar@yahoo.fr>
2024-02-15 20:34:25 +0100

    started implementing auto norm

--- script20240202_intensityevolutionin1area.py
+++ script20240202_intensityevolutionin1area.py
@@ -126,9 +126,10 @@ def intensity_evolution(list_of_images, x_range=None, y_range=None, remove_peaks
                 # Look for previous index to keep
                 last_true_index = i
                 while keep_index[last_true_index] == False and last_true_index > 0:
-                    last_true_index = last_true_index-1
+                    last_true_index = last_true_index - 1
                 # Update the 1st derivative according to the last valid point
-                first_derivative[i + 1] = avg_intensities[i + 2] - avg_intensities[last_true_index]  # out of bound check?
+                first_derivative[i + 1] = avg_intensities[i + 2] - avg_intensities[
+                    last_true_index]  # out of bound check?
 
         # Remove the peak values
         avg_intensities = avg_intensities[keep_index]
@@ -358,7 +359,7 @@ def interactive_intensity_plot(image_path, start, end, quarter, x_range=None, y_
             rightmost_ax.set_ylim(np.min(normalized_intensities), np.max(normalized_intensities))
 
             # Crappy test for identifying drops
-            curr_intensity_drops = find_drops(selected_pixel_intensities, -0.1)
+            curr_intensity_drops = find_drops(selected_pixel_intensities)
             drops_lines.set_segments(
                 update_vertical_line(drops_lines, curr_intensity_drops, y_min=rightmost_ax.get_ylim()[0],
                                      y_max=rightmost_ax.get_ylim()[1]))
@@ -382,6 +383,263 @@ def interactive_intensity_plot(image_path, start, end, quarter, x_range=None, y_
 
             # Update the vertical line corresponding to the pixel intensity on the left axis
             normalization_pixel_vertical_line.set_xdata([normalization_pixel_intensities[curr_time]])
+
+            # Update the normalized plot
+            normalized_intensities = selected_pixel_intensities / normalization_pixel_intensities
+            # normalized_intensities = selected_pixel_intensities / whole_image_intensities
+            normalized_intensity_plot[0].set_data(range(len(normalized_intensities)), normalized_intensities)
+
+            # Update plot limits
+            right_ax.set_ylim(
+                int(np.min([np.min(selected_pixel_intensities), np.min(normalization_pixel_intensities)])),
+                int(np.max([np.max(selected_pixel_intensities), np.max(normalization_pixel_intensities)])))
+            rightmost_ax.set_ylim(np.min(normalized_intensities), np.max(normalized_intensities))
+
+            # Crappy test for identifying drops
+            curr_intensity_drops = find_drops(selected_pixel_intensities)
+            drops_lines.set_segments(
+                update_vertical_line(drops_lines, curr_intensity_drops, y_min=rightmost_ax.get_ylim()[0],
+                                     y_max=rightmost_ax.get_ylim()[1]))
+
+        curr_fig = plt.gcf()
+        curr_fig.show()
+
+    fig.canvas.mpl_connect('button_press_event', click_event)
+
+    print("=== INSTRUCTION GUIDE! :-) ===")
+    print(
+        "In order to move through the trajectory slowly, use the mouse scroll. If it freezes, just wait, it's loading.")
+    print("In order to jump to a faraway timestep, use the slider below the graph.")
+    print("In order to look at the intensity evolution of a single pixel, left click on it.")
+    print("In order to switch back to full intensity profile, ")
+
+    plt.show()
+
+
+def find_non_visited_sites(silhouettes, dilation_radius, site_size):
+    """
+    Takes a list of worm silhouettes (time series of matrices, with tracking label of each pixel, 0 is background), a
+    radius by which to dilate those silhouettes (so a pixel that is this radius away from a silhouette will be
+    considered to have been visited), and a site size (an integer corresponding to half the side of a square site).
+    Will return a list of all the sites of the correct size that have never been too close to the silhouette.
+    """
+
+    # Sum all silhouette matrices because we want to find sites that have never been visited
+    silhouettes_all_times = np.sum(silhouettes, axis=0)
+
+
+
+
+
+def find_best_normalization(image_list, x_range, y_range, silhouettes):
+    """
+    Takes a list of images, an x and y range on that image (in the format of a list: x_range could be [100, 200]),
+    and will determine the best area of pixels to normalize the selected range on.
+    First method I'm going to implement: look for the closest area of the same size that has never been close to the
+    silhouettes.
+    """
+
+def interactive_intensity_plot_auto_normalization(image_path, start, end, quarter, x_range=None, y_range=None,
+                                                  analysis_size=10):
+    """
+        Function that shows a scrollable plot with on top, an area of the image, and on the bottom, the mean intensity
+        evolution for this area. When you scroll, it should switch to the next frame, and display current time on curve.
+        """
+    # Find .tif images in the defined folder
+    image_path_list = sorted(glob.glob(image_path + "*.tif", recursive=False))
+
+    # Adjust end time so that it does not exceed maximal size
+    end = min(end, len(image_path_list))
+
+    # Set x_range and y_range as full image boundaries if they're set to None
+    first_img = cv2.imread(image_path_list[quarter], -1)
+    if x_range is None:
+        x_range = [0, first_img.shape[1]]
+    if y_range is None:
+        y_range = [0, first_img.shape[0]]
+
+    # Select the images in image_path_list that correspond to the quarter defined in arguments
+    # (using parity and number of lines/columns in the microscope tiling)
+    global image_list, hist_list, bin_list
+    image_list = []
+    # Lists to store the histogram of each image
+    hist_list = []
+    bin_list = []
+    for i in range(start, end):
+        if i % (n_lines * n_columns) == quarter:
+            image = cv2.imread(image_path_list[i],
+                               -1)  # -1 parameter is to not ruin the .tif upon import!!! (bit depth is 16)
+            # image = cv2.addWeighted(image, 60, np.zeros(image.shape, image.dtype), 0, 100)
+            # image = 255*((image - min(0, np.min(image)))/np.max(image))
+            image = image[y_range[0]:y_range[1], x_range[0]:x_range[1]]
+            hist, bins = np.histogram(image, bins=1000)
+            hist_list.append(hist)
+            bin_list.append(bins)
+            image_list.append(image)
+
+    # Define figure and axes
+    global left_ax
+    global middle_ax
+    global right_ax
+    global rightmost_ax
+    fig, [left_ax, middle_ax, right_ax, rightmost_ax] = plt.subplots(1, 4)
+    fig.set_size_inches(25, 5)
+
+    # Define time counter (will be incremented/decremented depending on what user does)
+    global curr_time
+    curr_time = 0
+
+    # Left axis: histogram of intensity of the image, vertical line = selected pixel intensity in current frame
+    left_ax.set_yscale("log")
+    left_ax.set_title("Histogram of the current image")
+    global hist_plot
+    hist_plot = left_ax.plot(bin_list[0][0:-1], hist_list[0])
+    global selected_pixel_vertical_line
+    selected_pixel_vertical_line = left_ax.axvline(bin_list[0][1], color="darkorange")
+    global normalization_pixel_vertical_line
+    normalization_pixel_vertical_line = left_ax.axvline(bin_list[0][1], color="grey")
+
+    # Middle axis: current frame of the image. Little orange star = selected pixel
+    middle_ax.imshow(image_list[0])
+    middle_ax.set_title("Image at current time point")
+    # Selected pixel initialization: start with the middle of the image
+    image_middle_line = image_list[curr_time].shape[0]
+    image_middle_col = image_list[curr_time].shape[1]
+    global selected_pixel
+    selected_pixel = middle_ax.scatter([image_middle_line], [image_middle_col], color="orange", marker="s",
+                                       s=analysis_size * 2, zorder=3, alpha=0.6)
+    # Initialize plot object for a pixel to base the normalization on
+    global normalization_pixel
+    normalization_pixel = middle_ax.scatter([], [], color="grey", marker="s", s=analysis_size * 2, zorder=3, alpha=0.6)
+
+    # Right axis: evolution of average intensity of the selected pixels (orange), of the normalization pixels (grey)
+    right_ax.set_title("Evolution of average intensity of orange / grey pixels")
+    # Orange: selected pixel intensity evolution (averaged over a 20x20 square around it)
+    global selected_pixel_intensities, selected_pixel_intensity_plot
+    selected_pixel_intensities = intensity_evolution(image_list, [image_middle_col - analysis_size,
+                                                                  image_middle_col + analysis_size],
+                                                     [image_middle_line - analysis_size,
+                                                      image_middle_line + analysis_size])
+    selected_pixel_intensity_plot = right_ax.plot(range(len(selected_pixel_intensities)), selected_pixel_intensities,
+                                                  color="darkorange")
+    # Grey: normalization pixel intensity evolution
+    global normalization_pixel_intensities, normalization_pixel_intensity_plot
+    normalization_pixel_intensities = intensity_evolution(image_list, [image_middle_col - analysis_size,
+                                                                       image_middle_col + analysis_size],
+                                                          [image_middle_line - analysis_size,
+                                                           image_middle_line + analysis_size])
+    normalization_pixel_intensity_plot = right_ax.plot(range(len(normalization_pixel_intensities)),
+                                                       normalization_pixel_intensities, color="grey")
+    # Initialize the vertical line that should move depending on current time
+    global current_time_line_right
+    current_time_line_right = right_ax.axvline(curr_time, color="black")
+
+    global whole_image_intensities
+    whole_image_intensities = intensity_evolution(image_list)
+
+    # Rightmost axis: normalized evolution of intensity (selected pixel / normalization pixel)
+    rightmost_ax.set_title("Normalized intensity (orange divided by grey)")
+    # Green: selected pixel divided by normalization pixel (through time)
+    global normalized_intensities, normalized_intensity_plot
+    normalized_intensities = selected_pixel_intensities / normalization_pixel_intensities
+    normalized_intensity_plot = rightmost_ax.plot(range(len(normalized_intensities)), normalized_intensities,
+                                                  color="green")
+    # Red: vertical lines indicating where the algorithm detects that there has been an intensity drop
+    global drops_lines
+    drops_lines = rightmost_ax.vlines([], ymin=rightmost_ax.get_ylim()[0], ymax=rightmost_ax.get_ylim()[1], color="red")
+    # Initialize the vertical line that should move depending on current time
+    global current_time_line_rightmost
+    current_time_line_rightmost = rightmost_ax.axvline(curr_time, color="black")
+
+    # Call the update frame once to initialize the plot
+    update_frame(image_list, hist_list, bin_list, curr_time)
+
+    # Make the plot scrollable
+    def scroll_event(event):
+        global curr_time
+        if event.button == "up":
+            curr_time = curr_time + 1
+        elif event.button == "down":
+            curr_time = max(0, curr_time - 1)
+        else:
+            return
+        update_frame(image_list, hist_list, bin_list, curr_time)
+
+    fig.canvas.mpl_connect('scroll_event', scroll_event)
+
+    # Create a slider
+    global bottom_ax
+    fig = plt.gcf()
+    bottom_ax = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+    frame_slider = Slider(bottom_ax, 'Time', 0, len(image_list), valinit=0, color="green")
+    plt.subplots_adjust(left=0.25, bottom=.2, right=None, top=.9, wspace=.2, hspace=.2)
+
+    # Slider update function
+    def slider_update(val):  # val argument gives "unused" warning in PyCharm but is necessary
+        global curr_time
+        curr_time = int(frame_slider.val)
+        update_frame(image_list, hist_list, bin_list, curr_time)
+
+    # Slider function update call
+    frame_slider.on_changed(slider_update)
+
+    def click_event(event):
+        global image_list
+        global curr_time
+        global right_ax
+        global rightmost_ax
+
+        # Plot objects for the star representing those pixels on the image
+        global selected_pixel
+        global normalization_pixel
+
+        # Plot objects for vertical line corresponding to intensity of selected/norm pixels, and intensity drops
+        global selected_pixel_vertical_line
+        global normalization_pixel_vertical_line
+        global drops_lines
+
+        # List of intensities already computed for selected, normalization pixels and normalized version
+        global selected_pixel_intensities
+        global normalization_pixel_intensities
+        global normalized_intensities
+        global whole_image_intensities
+
+        # Plot objects for the curves representing the intensity evolution on the right plot
+        global selected_pixel_intensity_plot
+        global normalization_pixel_intensity_plot
+        global normalized_intensity_plot
+
+        if event.button is MouseButton.LEFT and event.inaxes == middle_ax:
+            # Get the mouse click position, make it integer
+            curr_x = int(event.xdata)
+            curr_y = int(event.ydata)
+            print(curr_x)
+            print(curr_y)
+
+            # Change the position of the dot
+            selected_pixel.set_offsets([curr_x, curr_y])
+
+            # Update the plot of evolution of intensity of the current selected pixel
+            selected_pixel_intensities = intensity_evolution(image_list, [curr_x - analysis_size,
+                                                                          curr_x + analysis_size],
+                                                             [curr_y - analysis_size,
+                                                              curr_y + analysis_size])
+            selected_pixel_intensity_plot[0].set_data(range(len(selected_pixel_intensities)),
+                                                      selected_pixel_intensities)
+
+            # Look for the best normalization pixels (closest area where the worm never went)
+            curr_norm_x, curr_norm_y = find_best_normalization([curr_x - analysis_size, curr_x + analysis_size],
+                                                               [curr_y - analysis_size, curr_y + analysis_size])
+            # Update the plot of evolution of intensity of the normalization pixel
+            normalization_pixel_intensities = intensity_evolution(image_list, [curr_norm_x - analysis_size,
+                                                                               curr_norm_x + analysis_size],
+                                                                  [curr_norm_y - analysis_size,
+                                                                   curr_norm_y + analysis_size])
+            normalization_pixel_intensity_plot[0].set_data(range(len(normalization_pixel_intensities)),
+                                                           normalization_pixel_intensities)
+
+            # Update the vertical line corresponding to the pixel intensity on the left axis
+            selected_pixel_vertical_line.set_xdata([selected_pixel_intensities[curr_time]])
 
             # Update the normalized plot
             normalized_intensities = selected_pixel_intensities / normalization_pixel_intensities
@@ -442,6 +700,7 @@ def update_frame(list_of_images, histogram_values, bin_edges, index):
 if __name__ == "__main__":
     # Paths to find images
     path = "/media/admin/Expansion/Backup/Patch_depletion_dissectoscope/20243101_OD0.2oldbact10%gfp4s_Lsomethingworm_dishupsidedown-02/"
+    silhouettes = np.load(path + "silhouettes.npy")
 
     # Number of lines and columns to the microscope tiling (for when you take only a quarter of the drop on each img for ex)
     n_lines = 2
@@ -458,7 +717,7 @@ if __name__ == "__main__":
     x_limits = None
     y_limits = None
 
-    interactive_intensity_plot(path, start_time, end_time, 2, x_limits, y_limits, analysis_size=10)
+    interactive_intensity_plot(path, silhouettes, start_time, end_time, 2, x_limits, y_limits, analysis_size=10)
 
 # intensities1 = []
 # intensities2 = []
