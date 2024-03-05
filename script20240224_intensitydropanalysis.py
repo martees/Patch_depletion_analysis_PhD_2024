42d4ebf2f00e499ccd6c5eae7366b57abf7b67c4
martees <asmar.contact@yahoo.com>
2024-03-05 20:49:26 +0100

    minor debugging, integration to main pipeline, added cumulated sum

--- script20240224_intensitydropanalysis.py
+++ script20240224_intensitydropanalysis.py
@@ -7,7 +7,7 @@ import cv2
 import copy
 
 
-def intensity_vs_wormed_frames(list_of_images, full_img_intensity_evolution, x, y):
+def intensity_vs_wormed_frames(list_of_images, full_img_intensity_evolution, x, y, start_time, end_time):
     """
     Will return a list of average intensities for the (x,y) pixel, corresponding to the average intensity between
     every worm passage to this pixel (represented by nans). If there are multiple consecutive nans, will put nans
@@ -33,45 +33,58 @@ def intensity_vs_wormed_frames(list_of_images, full_img_intensity_evolution, x, 
             average_each_passage[i_nan] = np.nan
         else:
             average_each_passage[i_nan] = np.nanmean(pixel_intensity_list[max(prev_nan_index, curr_nan_index - 10): curr_nan_index])
-            # average_each_passage[i_nan] /= np.nanmean(full_img_intensity_evolution[max(prev_nan_index, curr_nan_index - 10): curr_nan_index])
+            average_each_passage[i_nan] /= np.nanmean(full_img_intensity_evolution[max(prev_nan_index, curr_nan_index - 10): curr_nan_index])
         prev_nan_index = curr_nan_index
 
     # Make it be the difference with the previous period
+    # diff_each_passage has nan values whenever there are consecutive wormed time steps in average_each_passage, so that
+    # the i-th term of diff_each_passage has the difference in intensity after i time steps with the worm
     diff_each_passage = copy.copy(average_each_passage)
+    # Find indices where the intensity is defined (non nan values of average_each_passage)
     non_nan_indices = np.where(np.logical_not(np.isnan(diff_each_passage)))[0]
+    # Start at 1 because difference is defined for the second element onwards (we add 0 as the first after the loop)
     for i_not_nan in range(1, len(non_nan_indices)):
         curr_nan = non_nan_indices[i_not_nan]
         prev_nan = non_nan_indices[i_not_nan - 1]
         diff_each_passage[curr_nan] -= average_each_passage[prev_nan]
     diff_each_passage[0] = 0
 
-    if np.max(diff_each_passage) > 1:
-        print("hehe")
+    # if len(diff_each_passage) > 1:
+    #     print("hihi")
+
+    diff_each_passage = np.nancumsum(diff_each_passage)
 
     return average_each_passage, diff_each_passage
 
 
-def plot_intensity_worm_passages(path, timestamps):
-    image_list = intensity.no_worm_images(path + "assembled_images/", timestamps)
+def plot_intensity_worm_passages(path, start_time, end_time, timestamps, x_range=None, y_range=None):
+    print("Loading no_worm images...")
+    image_path_list = useful_functions.find_path_list(path + "assembled_images/")
+    image_list = [[] for _ in range(start_time, end_time)]
+    for t in range(start_time, end_time):
+        image_list[t - start_time] = np.load(image_path_list[t][:-len(".tif")] + "_nansilhouette.npy")
 
+    print("Computing average intensity in every image...")
     full_img_intensity_list = np.zeros(len(image_list))
-    for i in range(len(full_img_intensity_list)):
-        full_img_intensity_list[i] = np.mean(image_list[i][1600:2100][2000:2500])
+    for i in range(len(image_list)):
+        full_img_intensity_list[i] = np.nanmean(image_list[i][y_range[0]:y_range[1], x_range[0]:x_range[1]])
 
     list_of_avg_intensities = [[] for _ in range(len(timestamps))]
-    for x in range(2000, 2500):
+    for x in range(x_range[0], x_range[1]):
         if x % 100 == 0:
-            print("Computing intensity evolution for column ", x, " / ", 3004)
-        for y in range(1600, 2100):
-            intensity_series_this_pixel, intensity_diff_this_pixel = intensity_vs_wormed_frames(image_list, full_img_intensity_list, x, y)
+            print("Computing intensity evolution for column ", x, " / ", x_range[1])
+        for y in range(y_range[0], y_range[1]):
+            intensity_series_this_pixel, intensity_diff_this_pixel = intensity_vs_wormed_frames(image_list, full_img_intensity_list, x, y, start_time, end_time)
             for i in range(len(intensity_diff_this_pixel)):
                 if not np.isnan(intensity_diff_this_pixel[i]):
                     list_of_avg_intensities[i].append(intensity_diff_this_pixel[i])
 
     list_of_avg_intensities = [list_of_avg_intensities[i] for i in range(len(list_of_avg_intensities)) if list_of_avg_intensities[i]]
 
-    plt.boxplot(list_of_avg_intensities)
-    plt.ylabel("Average diff between when worm left and when worm arrived")
+    plt.boxplot(list_of_avg_intensities, showmeans=True, meanline=True)
+    plt.axhline(0, color="grey", linestyle="--")
+    plt.title("Cumulated sum of normalized worm-related intensity variations as a function of time near the worm")
+    plt.ylabel("Average normalized difference in intensity")
     plt.xlabel("Number of worm passages")
     plt.show()
 
